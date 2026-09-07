721d46ebcbbbeaac19bf1d010f2cf774e8630386
Jodaru <jodaru95@gmail.com>
2026-09-07 17:29:34 +0200

    Implementacion del sistema de cache con 1h de caducidad

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,20 +1,41 @@
 const API_URL = "https://itx-frontend-test.onrender.com";
+const PRODUCTS_CACHE_KEY = "products_cache";
+const CACHE_DURATION = 60 * 60 * 1000;
+
 
 export async function getProducts() {
-    const response = await fetch(API_URL+'/api/product');
+    const cached = localStorage.getItem(PRODUCTS_CACHE_KEY);
 
-    if(!response.ok){
+    if (cached) {
+        const now = Date.now();
+        const cache = JSON.parse(cached);
+
+        if (now - cache.timestamp < CACHE_DURATION) {
+            return cache.data;
+        }
+    }
+
+    const response = await fetch(API_URL + '/api/product');
+
+    if (!response.ok) {
         throw new Error("Error al intentar obtener los productos");
     }
 
     const data = await response.json();
+
+    const cache= {
+        data: data,
+        timestamp: Date.now()
+    };
+    localStorage.setItem(PRODUCTS_CACHE_KEY,JSON.stringify(cache));
+
     return data;
 }
 
-export async function getProductDetail(id){
-    const response = await fetch(API_URL+'/api/product/'+id);
+export async function getProductDetail(id) {
+    const response = await fetch(API_URL + '/api/product/' + id);
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error("Error al intentar obtener los datos del producto");
     }
 
